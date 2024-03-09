da482f755e80031ae4b43f0141858326753e47c4
mateuszprask <mateusz.prask@gmail.com>
2024-03-09 14:20:00 +0100

    update task02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredientsList = document.getElementById("ingredients");
+
+if (ingredientsList) {
+  const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatoes",
+    "Herbs",
+    "Condiments",
+  ];
+
+  const fragment = document.createDocumentFragment();
+
+  ingredients.forEach((ingredient) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = ingredient;
+    listItem.classList.add("item");
+    fragment.appendChild(listItem);
+  });
+
+  ingredientsList.appendChild(fragment);
+} else {
+  console.error('Element with id "ingredients" not found.');
+}
