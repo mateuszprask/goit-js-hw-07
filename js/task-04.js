cc07ed1977321ca8c6180972b791e6a370c99e03
mateuszprask <mateusz.prask@gmail.com>
2024-03-09 14:29:41 +0100

    uodate task04

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document
+  .querySelector("button[data-create]")
+  .addEventListener("click", function () {
+    const amount = document.querySelector("input").value;
+    createBoxes(amount);
+  });
+
+document
+  .querySelector("button[data-destroy]")
+  .addEventListener("click", destroyBoxes);
